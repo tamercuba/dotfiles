803c9765364abe69454649a36c9c4497675af121
Tamer Cuba <tamercuba@gmail.com>
2025-07-10 14:23:27 -0300

    fix: wrap=true

--- terminal/.config/nvim/lua/config/opts.lua
+++ terminal/.config/nvim/lua/config/opts.lua
@@ -11,7 +11,7 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.cmd("set number")
 vim.cmd("set colorcolumn=80,120")
