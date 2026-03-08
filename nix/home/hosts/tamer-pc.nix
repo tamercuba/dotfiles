9a8e9c8f280808c0a5b530a96557d440a7f228d4
Tamer Cuba <tamercuba@gmail.com>
2026-03-08 20:19:05 -0300

    feature: adding ln to my scripts

--- nix/home/hosts/tamer-pc.nix
+++ nix/home/hosts/tamer-pc.nix
@@ -11,5 +11,21 @@
     file.".ssh" = {
       source = config.lib.file.mkOutOfStoreSymlink "/mnt/storage/.ssh";
     };
+
+    file.".local/bin/toggle-audio-sink-clj" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/storage/projects/dotfiles/wayland/.local/bin/toggle-audio-sink-clj";
+    };
+
+    file.".local/bin/tmux_attach_clj" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/storage/projects/dotfiles/terminal/.local/bin/tmux_attach_clj";
+    };
+
+    file.".local/bin/tmux_auto_attach" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/storage/projects/dotfiles/terminal/.local/bin/tmux_auto_attach";
+    };
+
+    file.".local/bin/bb-nrepl" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/storage/projects/dotfiles/terminal/.local/bin/bb-nrepl";
+    };
   };
 }
