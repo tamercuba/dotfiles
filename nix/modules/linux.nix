6461dc3a3619a60411e966d002e2c54e3a3e675e
Tamer Cuba <tamercuba@gmail.com>
2026-03-09 14:03:26 -0300

    feature: improving some audio stuff

--- nix/modules/linux.nix
+++ nix/modules/linux.nix
@@ -53,6 +53,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.quantum = 512;
+        default.clock.min-quantum = 512;
+        default.clock.max-quantum = 512;
+      };
+    };
   };
 
 
