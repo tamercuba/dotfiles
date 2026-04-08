65bd97ae52d2d06b11166d6bad01237149bdb743
Tamer Cuba <tamercuba@gmail.com>
2026-04-08 15:21:00 -0300

    fix: configuring tree-sitter for nvim 0.12

--- terminal/.config/nvim/lua/plugins/treesitter.lua
+++ terminal/.config/nvim/lua/plugins/treesitter.lua
@@ -1,88 +1,26 @@
+local parsers = {
+	"bash", "c", "clojure", "html", "javascript", "typescript", "tsx",
+	"json", "lua", "luadoc", "luap", "query", "regex", "vim", "vimdoc",
+	"yaml", "rust", "go", "gomod", "gowork", "gosum", "nix", "markdown",
+	"markdown_inline",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
-		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 		config = function()
 			vim.treesitter.language.register("clojure", "risp")
 
-			require("nvim-treesitter").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"clojure",
-					"html",
-					"javascript",
-					"typescript",
-					"tsx",
-					"json",
-					"lua",
-					"luadoc",
-					"luap",
-					"query",
-					"regex",
-					"vim",
-					"vimdoc",
-					"yaml",
-					"rust",
-					"go",
-					"gomod",
-					"gowork",
-					"gosum",
-					"nix",
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						include_surrounding_whitespace = false,
-						keymaps = {
-							["af"] = { query = "@function.outer", desc = "around a function" },
-							["if"] = { query = "@function.inner", desc = "inner part of a function" },
-							["ac"] = { query = "@class.outer", desc = "around a class" },
-							["ic"] = { query = "@class.inner", desc = "inner part of a class" },
-							["ai"] = { query = "@conditional.outer", desc = "around an if statement" },
-							["ii"] = { query = "@conditional.inner", desc = "inner part of an if statement" },
-							["al"] = { query = "@loop.outer", desc = "around a loop" },
-							["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
-							["ap"] = { query = "@parameter.outer", desc = "around parameter" },
-							["ip"] = { query = "@parameter.inner", desc = "inside a parameter" },
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v",
-							["@parameter.inner"] = "v",
-							["@function.outer"] = "v",
-							["@conditional.outer"] = "V",
-							["@loop.outer"] = "V",
-							["@class.outer"] = "<c-v>",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]f"] = { query = "@function.outer", desc = "Next function" },
-							["]c"] = { query = "@class.outer", desc = "Next class" },
-							["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
-						},
-						goto_previous_start = {
-							["[f"] = { query = "@function.outer", desc = "Previous function" },
-							["[c"] = { query = "@class.outer", desc = "Previous class" },
-							["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>sn"] = { query = "@parameter.inner", desc = "Swap next parameter" },
-						},
-						swap_previous = {
-							["<leader>sp"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
-						},
-					},
-				},
-			})
+			local installed = require("nvim-treesitter.config").get_installed()
+			local to_install = vim.iter(parsers)
+				:filter(function(p) return not vim.tbl_contains(installed, p) end)
+				:totable()
+			if #to_install > 0 then
+				require("nvim-treesitter").install(to_install)
+			end
 
 			vim.api.nvim_create_autocmd("FileType", {
 				callback = function(ev)
@@ -94,5 +32,57 @@ return {
 			})
 		end,
 	},
-	{ "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		lazy = false,
+		config = function()
+			local ts = require("nvim-treesitter-textobjects")
+			ts.setup({
+				select = {
+					lookahead = true,
+					include_surrounding_whitespace = false,
+					selection_modes = {
+						["@parameter.outer"] = "v",
+						["@parameter.inner"] = "v",
+						["@function.outer"] = "v",
+						["@conditional.outer"] = "V",
+						["@loop.outer"] = "V",
+						["@class.outer"] = "<c-v>",
+					},
+				},
+				move = { set_jumps = true },
+			})
+
+			local select_to = require("nvim-treesitter-textobjects.select").select_textobject
+			local move = require("nvim-treesitter-textobjects.move")
+			local swap = require("nvim-treesitter-textobjects.swap")
+
+			local select_maps = {
+				["af"] = "@function.outer", ["if"] = "@function.inner",
+				["ac"] = "@class.outer", ["ic"] = "@class.inner",
+				["ai"] = "@conditional.outer", ["ii"] = "@conditional.inner",
+				["al"] = "@loop.outer", ["il"] = "@loop.inner",
+				["ap"] = "@parameter.outer", ["ip"] = "@parameter.inner",
+			}
+			for key, query in pairs(select_maps) do
+				vim.keymap.set({ "x", "o" }, key, function() select_to(query, "textobjects") end)
+			end
+
+			local move_maps = {
+				["]f"] = { fn = move.goto_next_start, query = "@function.outer" },
+				["]c"] = { fn = move.goto_next_start, query = "@class.outer" },
+				["]p"] = { fn = move.goto_next_start, query = "@parameter.inner" },
+				["[f"] = { fn = move.goto_previous_start, query = "@function.outer" },
+				["[c"] = { fn = move.goto_previous_start, query = "@class.outer" },
+				["[p"] = { fn = move.goto_previous_start, query = "@parameter.inner" },
+			}
+			for key, map in pairs(move_maps) do
+				vim.keymap.set({ "n", "x", "o" }, key, function() map.fn(map.query, "textobjects") end)
+			end
+
+			vim.keymap.set("n", "<leader>sn", function() swap.swap_next("@parameter.inner") end, { desc = "Swap next parameter" })
+			vim.keymap.set("n", "<leader>sp", function() swap.swap_previous("@parameter.inner") end, { desc = "Swap previous parameter" })
+		end,
+	},
 }
