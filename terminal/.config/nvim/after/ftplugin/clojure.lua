577c641edec3be37257af7628d06a8bbe080d5c3
Tamer Cuba <tamercuba@gmail.com>
2026-05-07 16:23:36 -0300

    feature: improving keybindings to add new form in clojure files

--- terminal/.config/nvim/after/ftplugin/clojure.lua
+++ terminal/.config/nvim/after/ftplugin/clojure.lua
@@ -4,3 +4,5 @@ vim.opt_local.iskeyword:remove(".")
 vim.bo.lisp = true
 vim.bo.autoindent = true
 vim.opt_local.lispwords:append("s/defn,s/def,s/defschema,s/defrecord")
+
+vim.keymap.set("i", "<C-n>", "()<Left>", { buffer = true, noremap = true })
