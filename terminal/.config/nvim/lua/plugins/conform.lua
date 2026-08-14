8a5234da5799039595ef3e6173610315f0a7fcbf
Tamer Cuba <tamercuba@gmail.com>
2026-08-14 10:22:26 -0300

    feature: add dartls LSP and treesitter parser, disable format-on-save for dart files

--- terminal/.config/nvim/lua/plugins/conform.lua
+++ terminal/.config/nvim/lua/plugins/conform.lua
@@ -14,7 +14,7 @@ local config = { -- Autoformat
 	opts = {
 		notify_on_error = true,
 		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = true, cpp = true, dart = true }
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
